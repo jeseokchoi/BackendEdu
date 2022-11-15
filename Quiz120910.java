b774ae5b47c622ab0c607aa5b6d89ce7cb03b830
spring51511 <111964440+spring51511@users.noreply.github.com>
2022-11-15 17:50:57 +0900

    프로그래머스_세균증식_김새봄_2

---
+++ Quiz120910.java
@@ -0,0 +1,19 @@
+package 프로그래머스;
+
+// 어떤 세균은 1시간에 두배만큼 증식한다고 합니다. 
+// 처음 세균의 마리수 n과 경과한 시간 t가 매개변수로 주어질 때 t시간 후 세균의 수를
+// return하도록 solution 함수를 완성해주세요.
+
+// 입출력의 예시
+// n	t	 result
+// 2	10	2048
+// 7	15	229,376
+
+class Solution {
+    public int solution(int n, int t) {
+        int answer = 0;
+   
+		answer = n;
+        return answer;
+    }
+}
