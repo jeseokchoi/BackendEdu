da39554202535f28e2e8bca6be39aea794213035
spring51511 <111964440+spring51511@users.noreply.github.com>
2022-11-15 17:27:17 +0900

    프로그래머스_예산+김새봄_1

---
+++ Ex01.java
@@ -0,0 +1,34 @@
+package 프로그래머스;
+
+import java.util.Arrays;
+
+public class Ex01 {
+	public static void main(String[] args) {
+
+		Solution sol = new Solution();
+		
+
+		System.out.println(sol.solution("Bcad"));// 229376
+
+	}
+
+}
+
+class Solution {
+	public String solution(String my_string) {
+		String answer = "";
+
+		my_string = my_string.toLowerCase();
+		String[] arr = my_string.split("");
+		Arrays.sort(arr);
+		
+		for(int i = 0 ; i <arr.length; i++) {
+		
+			answer += arr[i];
+			
+			
+		}
+
+		return answer;
+	}
+}
