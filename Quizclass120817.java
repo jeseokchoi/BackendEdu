12531621fd9f207ccfd5ab462ae5abb06c1e3e01
g960226 <117978684+g960226@users.noreply.github.com>
2022-11-15 17:38:05 +0900

    Add files via upload

---
+++ Quizclass120817.java
@@ -0,0 +1,15 @@
+package programmers;
+// 정수 배열 numbers가 매개변수로 주어집니다. 
+// numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+// 0 ≤ numbers의 원소 ≤ 1,000
+// 1 ≤ numbers의 길이 ≤ 100
+// 정답의 소수 부분이 .0 또는 .5인 경우만 입력으로 주어집니다.
+
+
+public class Quizclass120817 {
+	    public double solution(int[] numbers) {
+	        double answer = 0;
+	        return answer;
+	    }
+	}
+
